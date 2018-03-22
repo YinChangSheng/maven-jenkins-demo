0a7cea9dca4e0710722f5be288477a44dddc060a
yinchangsheng <ychangsheng@gmail.com>
2018-03-22 15:14:50 +0800

    项目创建

---
+++ src/main/java/com/yinchangsheng/demo1/AppHaha.java
@@ -0,0 +1,12 @@
+package com.yinchangsheng.demo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppHaha
+{
+    public void say() {
+        System.out.println("haha im from nexus 仓库 - 再来一发");
+    }
+}
